2b71b218d2d0940f4420cf78e6906f5765df1641
Marion Dorsett <marion.dorsett@gmail.com>
2024-01-19 17:24:19 -0500

    Added verification and automatic verification when creating a RESTHook subscriptions

--- src/genius-ksdk.php
+++ src/genius-ksdk.php
@@ -60,9 +60,13 @@ class GeniusKSDK {
      * }
      * 
      * @param string $payload JSON
+     * @param bool $verify Automatically verify the resthook subscription.
      * @return stdClass Object
      */
-    public function createRestHook(string $payload) {
+    public function createRestHook(string $payload, bool $verify = true) {
+        if ($verify === true) {
+            $this->verifyRestHook();
+        }
         return $this->request('/v1/hooks', array(
                     'method' => 'POST',
                     'header' => 'Content-Type: application/json',
@@ -99,6 +103,25 @@ class GeniusKSDK {
         return $this->request('/v1/hooks/' . $key, array(
                     'method' => 'DELETE',
         ));
+    }
+
+    /**
+     * Verify a Hook Subscription
+     * 
+     * @return mixed null|string
+     */
+    public function verifyRestHook() {
+        if (getenv('REQUEST_METHOD') !== 'POST') {
+            return;
+        }
+        $header = $this->requestHeaders();
+        $XHookSecret = ((isset($header['X-Hook-Secret'])) ? $header['X-Hook-Secret'] : null);
+        if ($XHookSecret === null) {
+            return;
+        }
+        header('X-Hook-Secret: ' . $XHookSecret);
+        echo $this->requestBody();
+        exit;
     }
 
     /**
@@ -209,5 +232,45 @@ class GeniusKSDK {
             'header' => null,
             'content' => null
         );
+    }
+
+    /**
+     * Load the request headers from the remote host.
+     * @return array
+     */
+    private function requestHeaders() {
+        return ((function_exists('apache_request_headers')) ? apache_request_headers() : $this->buildRequestHeadersFromServer());
+    }
+
+    /**
+     * https://www.php.net/manual/en/function.apache-request-headers.php#70810
+     * 
+     * If the apache_request_headers() function doesn't exist, use this function
+     * which mimics apache_request_headers() instead. - limalopex.eisfux.de
+     */
+    private function buildRequestHeadersFromServer() {
+        $arh = array();
+        $rx_http = '/\AHTTP_/';
+        foreach ($_SERVER as $key => $val) {
+            if (preg_match($rx_http, $key)) {
+                $arh_key = preg_replace($rx_http, '', $key);
+                $rx_matches = array();
+                // do some nasty string manipulations to restore the original letter case
+                // this should work in most cases
+                $rx_matches = explode('_', $arh_key);
+                if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
+                    foreach ($rx_matches as $ak_key => $ak_val)
+                        $rx_matches[$ak_key] = ucfirst($ak_val);
+                    $arh_key = implode('-', $rx_matches);
+                }
+                $arh[$arh_key] = $val;
+            }
+        }
+        $arh['Function'] = true;
+        return( $arh );
+    }
+
+    private function requestBody() {
+        return file_get_contents('php://input');
     }
 }
