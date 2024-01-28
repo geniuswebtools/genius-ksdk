f4a52d3556f3878af81b84a5fb12c81442b8e4ea
Marion Dorsett <marion.dorsett@gmail.com>
2024-01-27 20:00:29 -0500

    Added API models for REST and XML that provide full access to the respective APIs even if a helper model doesn't exist yet.

---
+++ src/geniusksdk/rest/api.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace GeniusKSDK\REST;
+
+/**
+ * This class provides full access to the REST API.
+ */
+class API extends \GeniusKSDK\REST {
+
+    public function __construct(\GeniusKSDK $client) {
+        parent::__construct($client);
+    }
+
+    /**
+     * Create an object
+     * 
+     * @param string $path
+     * @param array $struct
+     * @return stdClass Object
+     */
+    public function create(string $path, array $struct) {
+        $payload = json_encode($struct);
+        return $this->client->request($path, array(
+                    'method' => 'POST',
+                    'header' => array('Content-Type: application/json'),
+                    'content' => $payload,
+        ));
+    }
+
+    /**
+     * Read an object or Retrieve a list of objects
+     * 
+     * @param string $path
+     * @return stdClass Object
+     */
+    public function read(string $path) {
+        return $this->client->request($path);
+    }
+
+    /**
+     * Update an object
+     * 
+     * @param string $path
+     * @param array $struct
+     * @return stdClass Object
+     */
+    public function update(string $path, array $struct) {
+        $payload = json_encode($struct);
+        return $this->client->request($path, array(
+                    'method' => 'PATCH',
+                    'header' => array('Content-Type: application/json'),
+                    'content' => $payload,
+        ));
+    }
+
+    /**
+     * Delete an object
+     * 
+     * @param string $path
+     * @return stdClass Object
+     */
+    public function delete(string $path) {
+        return $this->client->request($path, array(
+                    'method' => 'DELETE',
+        ));
+    }
+}
---
+++ src/geniusksdk/xml/api.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GeniusKSDK\XML;
+
+/**
+ * This class provides full access to the XML-RPC API.
+ */
+class API extends \GeniusKSDK\XML {
+
+    public function __construct(\GeniusKSDK $client) {
+        parent::__construct($client);
+    }
+
+    /**
+     * This method provides full access to the  XML-RPC API.
+     */
+    public function call(string $service, array $struct) {
+        return $this->send($service, $struct);
+    }
+}
