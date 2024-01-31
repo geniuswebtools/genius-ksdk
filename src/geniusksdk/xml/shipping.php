42dc5ff2520172de692235bb08faed5fdc37aae7
Marion Dorsett <marion.dorsett@gmail.com>
2024-01-30 22:04:41 -0500

    Updated remaining services for XML shipping service.

--- src/geniusksdk/xml/shipping.php
+++ src/geniusksdk/xml/shipping.php
@@ -8,7 +8,41 @@ class Shipping extends \GeniusKSDK\XML {
         parent::__construct($client);
     }
 
-    public function list() {
+    public function list(string $type = 'all ') {
         return $this->send('ShippingService.getAllShippingOptions', array());
+    }
+
+    public function read(string $type, int $id) {
+        $params = array(
+            $id,
+        );
+        switch (strtolower($type)) {
+            case 'ups';
+                $service = 'ShippingService.getUpsShippingOption';
+                break;
+            case 'ordertotalranges':
+                $service = 'ShippingService.getOrderTotalShippingRanges';
+                break;
+            case 'orderqty':
+                $service = 'ShippingService.getOrderQuantityShippingOption';
+                break;
+            case 'ordertotal':
+                $service = 'ShippingService.getOrderTotalShippingOption';
+                break;
+            case 'product':
+                $service = 'ShippingService.getProductBasedShippingOption';
+                break;
+            case 'flat':
+                $service = 'ShippingService.getFlatRateShippingOption';
+                break;
+            case 'weight':
+                $service = 'ShippingService.getWeightBasedShippingOption';
+                break;
+            default:
+                throw new \Exception('Invalid $type provided.');
+                break;
+        }
+
+        return $this->send($service, $params);
     }
 }
