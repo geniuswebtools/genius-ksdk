0d4180c317f2e611b21ba19e3bd0eb58f24d8af3
Marion Dorsett <marion.dorsett@gmail.com>
2024-06-14 13:02:50 -0400

    Added CONST for unsupported method calls.

--- src/geniusksdk/xml.php
+++ src/geniusksdk/xml.php
@@ -7,6 +7,9 @@ abstract class XML {
     use \GeniusKSDK\Quirk;
 
     protected $client;
+    
+    CONST EX_UNSUPPORTED = '%s is not supported.';
+
 
     public function __construct(\GeniusKSDK $client) {
         $this->client = $client;
