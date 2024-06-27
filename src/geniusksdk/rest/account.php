bbba4faa3405441ee72430d57fd34b9281b4da2f
Marion Dorsett <marion.dorsett@gmail.com>
2024-06-27 13:48:58 -0400

    wrapped comments

--- src/geniusksdk/rest/account.php
+++ src/geniusksdk/rest/account.php
@@ -8,9 +8,14 @@ namespace GeniusKSDK\REST;
  * 
  * Bugs Submitted 2024-06-14:
  * Keap Case # 03313290: REST V1 Update Account Profile Bugs
- * 1) The /v1/account/profile endpoint accepts PUTs with almost no information - namely { "address": { "field": null } }, and will overwrite all non-specified fields as null with the exception of  time_zone, logo_url, currency_code, language_tag, and business_goals.
- * 2) The account profile address always returns "OTHER" regardless of input, which is at odds with the example in our documentation that shows "BILLING".
- * 3) As you pointed out, the endpoint only accepts ISO-3/ALPHA-3 input, yet only provides ISO-2/ALPHA-2 output.
+ * 1) The /v1/account/profile endpoint accepts PUTs with almost no information 
+ *    - namely { "address": { "field": null } }, and will overwrite all 
+ *    non-specified fields as null with the exception of  time_zone, logo_url, 
+ *    currency_code, language_tag, and business_goals.
+ * 2) The account profile address always returns "OTHER" regardless of input, 
+ *    which is at odds with the example in our documentation that shows "BILLING".
+ * 3) As you pointed out, the endpoint only accepts ISO-3/ALPHA-3 input, yet 
+ *    only provides ISO-2/ALPHA-2 output.
  * 
  * Resolution:
  * Use the v2 Business Profile endpoints:
